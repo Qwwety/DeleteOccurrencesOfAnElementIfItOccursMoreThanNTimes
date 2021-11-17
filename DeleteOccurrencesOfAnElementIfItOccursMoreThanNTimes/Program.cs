ac2030732f04e50654a79b8e99b53c22d27cc422
Qwwety <80448769+Qwwety@users.noreply.github.com>
2021-11-17 21:00:28 +0300

    Initial commit

---
+++ DeleteOccurrencesOfAnElementIfItOccursMoreThanNTimes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeleteOccurrencesOfAnElementIfItOccursMoreThanNTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dfd=DeleteNth(new int[] { 2, 2, 1, 2, 2, 3, 2, 2, 1, 2 }, 2);
+            Console.WriteLine(dfd);
+        }
+
+        public static int[] DeleteNth(int[] arr, int x)
+        {
+            List<int> list = new List<int>(arr);
+            int Count = 0;
+            foreach (int Num in list.ToArray())
+            {
+
+                for (int i=0;i< list.Count;i++)
+                {
+                    if (list[i] == Num)
+                    {
+                        Count++;
+                    }
+
+                    if (Count > x)
+                    {
+                        list.RemoveAt(i);
+                        break;
+                    }
+                }
+                Count = 0;
+
+            }
+            int[] Oleg = list.ToArray();
+            return Oleg;
+        }
+    }
+}
